d8df14f9f31bc7b642fdf73189b84a0ce704a357
oicu <oicu.cc@163.com>
2020-04-22 15:35:31 +0800

    合并用户查询脚本

--- wg-users.sh
+++ wg-users.sh
@@ -5,13 +5,13 @@ if [ $line -eq 0 ]; then
     echo "Not found any user.conf"
     exit 1
 fi
+
 list=$(grep "172.16.0.*24" clients/*.conf 2>/dev/null | \
     sed -e "s#clients/##g" \
         -e "s#.conf:Address = #\t#g" \
         -e "s#/24##g" | \
     awk '{print $2,"\t",$1}' | \
     sort -n -t "." -k 4)
-echo "$list"
 
 if [ "$1" = "-v" ]; then
     file="$(wg | sed -e '/^peer/d' -e '/endpoint/d' -e 's#/32##' -e 's#allowed ips: ##')"
@@ -22,4 +22,6 @@ if [ "$1" = "-v" ]; then
         file=$(echo "$file" | sed 's#'$IP'#'$IP'\t'$USER'#g')
         [ $icount = $line ] && echo "$file"
     done
+else
+    echo "$list"
 fi
